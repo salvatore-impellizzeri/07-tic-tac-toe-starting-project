494f1c8f21fe22e1026874d1a2dfa1d04d420b65
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2024-12-10 18:33:50 +0100

    Create/Creata Gameboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import Player from './components/Player.jsx';
+import GameBoard from './components/GameBoard.jsx';
+import { useState } from 'react';
 
 function App() {
+  const [activePlayer, setActivePLayer] = useState('X');
+
+  function handleSelectSquare() {
+    setActivePLayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+  }
 
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initalName={"Player 1"} symbol={"X"} />
-          <Player initalName={"Player 2"} symbol={"O"} />
+        <ol id="players" className='highlight-player'>
+          <Player initalName={"Player 1"} symbol={"X"} isActive={activePlayer === 'X'}/>
+          <Player initalName={"Player 2"} symbol={"O"} isActive={activePlayer === 'O'}/>
         </ol>
 
-        GAME BOARD
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
       </div>
 
       LOG
